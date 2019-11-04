fb18188299b33e731ba5c8b499357346b85cbf47
Ruiyang Ding <dry950526@gmail.com>
2019-11-04 15:47:05 +0100

    control with c

---
+++ Server/cmake-build-debug/CMakeFiles/Server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Server.dir/src/tcp_command.c.o"
+  "CMakeFiles/Server.dir/src/control.c.o"
+  "Server.pdb"
+  "Server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
